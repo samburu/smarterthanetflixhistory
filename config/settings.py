4f9ed97666b5dedfdd8acbb04f3441b47f4f04f6
Peter Mburu <peter.mburu@kimetrica.com>
2025-09-28 18:05:27 +0300

    Prepare for deployment on Render

--- config/settings.py
+++ config/settings.py
@@ -13,6 +13,7 @@ https://docs.djangoproject.com/en/5.2/ref/settings/
 from pathlib import Path
 import os
 from datetime import timedelta
+import dj_database_url
 
 
 TMDB_API_KEY = os.getenv("TMDB_API_KEY", "ead0d44cdeee1f348b4b4b9b56498041")
